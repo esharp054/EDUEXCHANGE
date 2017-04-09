b83c38c1b679e296dbe0d006846e5e4c76c76892
chrisasmussen <ccasmussen@smu.edu>
2017-04-09 13:51:31 -0500

    Newest working mysql

--- script.sql
+++ script.sql
@@ -1,50 +1,54 @@
 DROP DATABASE IF EXISTS eduexchange
 
-CREATE DATABASE eduexchange
+CREATE DATABASE eduexchange;
 
-USE eduexchange
+USE eduexchange;
 
 CREATE TABLE users (
-    first VARCHAR(20), 
-    last VARCHAR(20), 
-    email VARCHAR(40), 
-    phone VARCHAR(14), 
-    pass CHAR(64), 
-    avatar IMAGE BLOB, 
+    firstname VARCHAR(20) NOT NULL, 
+    lastname VARCHAR(20) NOT NULL, 
+    email VARCHAR(40) NOT NULL, 
+    phone VARCHAR(14) NOT NULL, 
+    pass CHAR(64) NOT NULL, 
+    avatar BLOB, 
     rating INTEGER,
-    userID INTEGER
+    userID INTEGER NOT NULL UNIQUE,
+    PRIMARY KEY (userID)
 );
 
 CREATE TABLE textbooks (
-    ibsn INTEGER, 
-    cover IMAGE BLOB, 
-    title VARCHAR(20), 
+    ibsn INTEGER NOT NULL, 
+    cover BLOB NOT NULL, 
+    title VARCHAR(20) NOT NULL, 
     description VARCHAR(20), 
-    upload_date DATE,
-    uploader INTEGER,
-    price INTEGER, 
-    class VARCHAR(20), 
-    stat TINYINT
+    upload_date DATE NOT NULL,
+    uploader INTEGER NOT NULL,
+    price INTEGER NOT NULL, 
+    class VARCHAR(20) NOT NULL, 
+    stat TINYINT NOT NULL,
+    PRIMARY KEY (ibsn, uploader)
 );
 
 CREATE TABLE notes ( 
-    cover IMAGE BLOB, 
-    title VARCHAR(20), 
-    description VARCHAR(20), 
-    upload_date DATE,
-    uploader INTEGER,
-    price INTEGER, 
-    class VARCHAR(20), 
-    stat TINYINT
+    cover BLOB NOT NULL, 
+    title VARCHAR(20) NOT NULL, 
+    description VARCHAR(20) NOT NULL, 
+    upload_date DATE NOT NULL,
+    uploader INTEGER NOT NULL,
+    price INTEGER NOT NULL, 
+    class VARCHAR(20) NOT NULL, 
+    stat TINYINT NOT NULL,
+    PRIMARY KEY (title, uploader)
 );
 
 CREATE TABLE supplies ( 
-    cover IMAGE BLOB, 
-    title VARCHAR(20), 
-    description VARCHAR(20), 
-    upload_date DATE, 
-    uploader INTEGER,
-    price INTEGER, 
-    class VARCHAR(20), 
-    stat TINYINT
+    cover BLOB NOT NULL, 
+    title VARCHAR(20) NOT NULL, 
+    description VARCHAR(20) NOT NULL, 
+    upload_date DATE NOT NULL, 
+    uploader INTEGER NOT NULL,
+    price INTEGER NOT NULL, 
+    class VARCHAR(20) NOT NULL, 
+    stat TINYINT NOT NULL,
+    PRIMARY KEY (title, uploader)
 );
