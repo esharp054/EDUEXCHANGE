169d6f7c7cf8cce3b8f79614f22bb5f809883077
chrisasmussen <ccasmussen@smu.edu>
2017-04-09 14:50:00 -0500

    Auto-poplulate users table

---
+++ autopopulate.sql
@@ -0,0 +1,32 @@
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User1', 'User', 'user1@smu.edu', '555-555-5550', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User2', 'User', 'user2@smu.edu', '555-555-5551', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User3', 'User', 'user3@smu.edu', '555-555-5552', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User4', 'User', 'user4@smu.edu', '555-555-5553', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User5', 'User', 'user5@smu.edu', '555-555-5554', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User6', 'User', 'user6@smu.edu', '555-555-5555', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User7', 'User', 'user7@smu.edu', '555-555-5556', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User8', 'User', 'user8@smu.edu', '555-555-5557', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User9', 'User', 'user9@smu.edu', '555-5555-5558', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User10', 'User', 'user10@smu.edu', '555-555-5559', 'PASSWORD', NULL, NULL, NULL);
+
+INSERT INTO `eduexchange`.`users` (`firstname`, `lastname`, `email`, `phone`, `pass`, `avatar`, `rating`, `userID`) 
+VALUES ('User11', 'User', 'user11@smu.edu', '555-555-5560', 'PASSWORD', NULL, NULL, NULL);
