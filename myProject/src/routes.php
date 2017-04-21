393b10d63c7d0c6b9f8bc93a838611b5f2c9d196
1 <a1@1deMacBook-Pro.local>
2017-04-20 19:32:30 -0500

    textbooks endpoints done

--- myProject/src/routes.php
+++ myProject/src/routes.php
@@ -1,70 +1,5 @@
 <?php
 // Routes
-$app->post('/signup',function($request,$response){
-
-		session_start();
-		if(isset($_SESSION["login"]) && $_SESSION["login"] === true){
-			return $this->response->withStatus(201);
-		}
-		else{
-		$input = $request->getParsedBody();
-        $stmt = $this->db->prepare("INSERT into users(username,password,email,phone) VALUES (:username, :password,:email,:phone)");
-        $pass = $input["password"];
-        $pass = md5($pass);
-        $stmt->bindValue(':username', $input["username"],PDO::PARAM_STR);
-        $stmt->bindValue(':password', $pass, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $input["email"], PDO::PARAM_STR);
-        $stmt->bindValue(':phone', $input["phone"], PDO::PARAM_INT);
-        $stmt->execute();
-        //$rows = $stmt->fetchAll();
-        $to = $input["email"];
-		$subject = "My subject";
-		$txt = "Hello world!";
-		$headers = "From:Eduexchange";
-		mail($to,$subject,$txt,$headers,"-feduexchange@smu.edu");
-
-        return $this->response->withJson($rows);
-        }
-});
-<<<<<<< HEAD
-
-
-$app-get('/textbooks',function($request,$response){
-
-    $stmt = $this->db->prepare("SELECT * FROM textbooks ORDER BY title");
-    $stmt -> execute();
-    $textbooks = $stmt->fetchAll();
-    return $this->response->withJson($textbooks);
-});
-
-$app-get('/notes',function($request,$response){
-
-    $stmt = $this->db->prepare("SELECT * FROM notes ORDER BY title");
-    $stmt -> execute();
-    $notes = $stmt->fetchAll();
-    return $this->response->withJson($notes);
-});
-
-$app-get('/supplies',function($request,$response){
-
-    $stmt = $this->db->prepare("SELECT * FROM supplies ORDER BY title");
-    $stmt -> execute();
-    $supplies = $stmt->fetchAll();
-    return $this->response->withJson($supplies);
-});
-
-
-
-
-
-
-
-
-
-
-
-//code below doesn't relate to our current project, just ignore it.
-=======
 $app->post('/signin',function($request,$response){
 
 		$input = $request->getParsedBody();
@@ -82,11 +17,149 @@ $app->post('/signin',function($request,$response){
 			return $this->response->withStatus(400);
 		}
 		return $this->response->withJson($userInfo);
-		
-		
 	
-});        
->>>>>>> origin/DBENDPOINTS
+});  
+
+$app->post('/signup',function($request,$response){
+
+		session_start();
+		if(isset($_SESSION["login"]) && $_SESSION["login"] === true){
+			return $this->response->withStatus(201);
+		}
+		else{
+		$input = $request->getParsedBody();
+        $stmt = $this->db->prepare("INSERT into users(username,password,email,phone) VALUES (:username, :password,:email,:phone)");
+        $pass = $input["password"];
+        $pass = md5($pass);
+        $stmt->bindValue(':username', $input["username"],PDO::PARAM_STR);
+        $stmt->bindValue(':password', $pass, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $input["email"], PDO::PARAM_STR);
+        $stmt->bindValue(':phone', $input["phone"], PDO::PARAM_INT);
+        try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+        $to = $input["email"];
+		$subject = "My subject";
+		$txt = "Hello world!";
+		$headers = "From:Eduexchange";
+		mail($to,$subject,$txt,$headers,"-feduexchange@smu.edu");    
+        return $this->response->withStatus(200);       
+});
+
+
+$app->get('/textbooks',function($request,$response){
+
+    $stmt = $this->db->prepare("SELECT * FROM textbooks ORDER BY title");
+    try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+    $textbooks = $stmt->fetchAll();
+    return $this->response->withJson($textbooks);
+});
+
+$app->get('/textbooks/[{id}]',function($request,$response,$arg){
+	
+	$id = $arg["id"];
+	$stmt = $this->db->prepare("SELECT * FROM textbooks WHERE textbook_id = :id");
+	$stmt->bindValue(':id',$id,PDO::PARAM_STR);
+	try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+	$textbook = $stmt->fetchAll();
+	return $this->response->withJson($textbook);
+});
+$app->get('/textbooks/[{class}]', function($request,$response,$arg){
+	
+	$class = $arg["class"];
+	$stmt = $this->db->prepare("SELECT * FROM textbooks WHERE class = :class");
+	$stmt->bindValue(':class', $class, PDO::PARAM_STR);
+	try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+	$textbook = $stmt->fetchAll();
+	return $this->response->withJson($textbook);
+	
+});
+$app->post('/textbooks',function($request,$response){
+	$input = $request->getParsedBody();
+	$stmt = $this->db->prepare("INSERT into textbooks(title, description,price,class,uploader_id) VALUES (:title,:description,:price,:class,:uploader_id)");
+	$stmt->bindValue(':title', $input["title"],PDO::PARAM_STR);
+	$stmt->bindValue(':description', $input["description"],PDO::PARAM_STR);	
+	$stmt->bindValue(':price', $input["price"],PDO::PARAM_STR);
+	$stmt->bindValue(':class', $input["class"],PDO::PARAM_STR);
+	$stmt->bindValue(':uploader_id', $input["uploader_id"],PDO::PARAM_STR);
+	try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+	return $this->response->withStatus(200);
+		
+	});
+$app->post('/textbooks/[{id}]',function($request,$response,$arg){
+	$id = $arg["id"];
+	$input = $request->getParsedBody();
+	$stmt = $this->db->prepare("UPDATE textbooks(title, description,price,class,uploader_id) VALUES (:title,:description,:price,:class,:uploader_id)");
+	$stmt->bindValue(':title', $input["title"],PDO::PARAM_STR);
+	$stmt->bindValue(':description', $input["description"],PDO::PARAM_STR);	
+	$stmt->bindValue(':price', $input["price"],PDO::PARAM_STR);
+	$stmt->bindValue(':class', $input["class"],PDO::PARAM_STR);
+	$stmt->bindValue(':uploader_id', $input["uploader_id"],PDO::PARAM_STR);
+	try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+	return $this->response->withStatus(200);
+	
+});
+$app->delete('/textbooks/[{id}]',function($request,$response,$arg){
+	$id = $arg["id"];
+	$stmt = $this->db->prepare("DELETE FROM textbooks WHERE textbook_id = :id");
+	$stmt->bindValue(':id',$id,PDO::PARAM_STR);
+	try{
+        	$stmt->execute();
+        }
+        catch(PDOException $e){
+        		return $this->response->withStatus(400);
+        }
+	return $this->response->withStatus(200);
+	
+});
+$app->get('/notes',function($request,$response){
+
+    $stmt = $this->db->prepare("SELECT * FROM notes ORDER BY title");
+    $stmt -> execute();
+    $notes = $stmt->fetchAll();
+    return $this->response->withJson($notes);
+});
+
+$app->get('/supplies',function($request,$response){
+
+    $stmt = $this->db->prepare("SELECT * FROM supplies ORDER BY title");
+    $stmt -> execute();
+    $supplies = $stmt->fetchAll();
+    return $this->response->withJson($supplies);
+});
+
+
+//--------------
+//--------------
+//--------------      
 $app->get('/todos',function($request,$response,$args){
 	$sth = $this->db->prepare("SELECT * FROM tasks ORDER BY task");
 	$sth -> execute();
