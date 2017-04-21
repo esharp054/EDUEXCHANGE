e639c8d6204f7c26a99f48252ffda9f26d215434
root <root@zero-to-slim.dev>
2017-04-21 01:08:50 +0000

    supply routes

---
+++ autopopulateNotes.sql
@@ -0,0 +1,32 @@
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Some note', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'This', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Is', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'A', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Test', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Dummy', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Data', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'For', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Our', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'Database', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
+
+INSERT INTO `eduexchange`.`notes` (`note_id`, `title`, `description`, `upload_date`, `viewDate`, `uploader_id`, `price`, `class`, `stat`) 
+VALUES (NULL, 'project', 'notes for DB', '2012-06-18 10:34:09', '2017-04-20 04:20:09', 1, 10, 'Databases', 1);
