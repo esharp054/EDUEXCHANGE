14921578c7889b1aa0f17e2b4920a0591bedf3f8
chrisasmussen <ccasmussen@smu.edu>
2017-04-20 19:49:48 -0500

    Newly updated tables and data

--- script.sql
+++ script.sql
@@ -4,6 +4,13 @@ CREATE DATABASE eduexchange;
 
 USE eduexchange;
 
+CREATE TABLE logins (
+    loginStatus BIT(1) NOT NULL,
+    email VARCHAR(40) NOT NULL UNIQUE,
+    pass CHAR(64) NOT NULL, 
+    lastLogin DATE NOT NULL
+);
+
 CREATE TABLE users (
     firstname VARCHAR(20) NOT NULL, 
     lastname VARCHAR(20) NOT NULL, 
@@ -13,42 +20,53 @@ CREATE TABLE users (
     avatar BLOB, 
     rating INTEGER,
     userID INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+    joined_date DATETIME NOT NULL,
     PRIMARY KEY (userID)
 );
 
 CREATE TABLE textbooks (
-    ibsn INTEGER NOT NULL, 
+    textbook_id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+    ibsn INTEGER, 
     cover BLOB NOT NULL, 
     title VARCHAR(20) NOT NULL, 
     description VARCHAR(20), 
-    upload_date DATE NOT NULL,
-    uploader INTEGER NOT NULL,
+    upload_date DATETIME NOT NULL,
+    viewDate DATETIME NOT NULL,
+    uploader_id INTEGER NOT NULL,
     price INTEGER NOT NULL, 
     class VARCHAR(20) NOT NULL, 
-    stat TINYINT NOT NULL,
-    PRIMARY KEY (ibsn, uploader)
+    stat BIT(1) NOT NULL,
+    PRIMARY KEY (ibsn, uploader_id),
+    CHECK (ibsn > 0),
+    CHECK (price > 0)
 );
 
 CREATE TABLE notes ( 
+    note_id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
     cover BLOB NOT NULL, 
     title VARCHAR(20) NOT NULL, 
     description VARCHAR(20) NOT NULL, 
-    upload_date DATE NOT NULL,
-    uploader INTEGER NOT NULL,
+    upload_date DATETIME NOT NULL,
+    viewDate DATETIME NOT NULL,
+    uploader_id INTEGER NOT NULL,
     price INTEGER NOT NULL, 
     class VARCHAR(20) NOT NULL, 
-    stat TINYINT NOT NULL,
-    PRIMARY KEY (title, uploader)
+    stat BIT(1) NOT NULL,
+    PRIMARY KEY (title, uploader_id),
+    CHECK (price > 0)
 );
 
 CREATE TABLE supplies ( 
+    supply_id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
     cover BLOB NOT NULL, 
     title VARCHAR(20) NOT NULL, 
     description VARCHAR(20) NOT NULL, 
-    upload_date DATE NOT NULL, 
-    uploader INTEGER NOT NULL,
+    upload_date DATETIME NOT NULL,
+    viewDate DATETIME NOT NULL,
+    uploader_id INTEGER NOT NULL,
     price INTEGER NOT NULL, 
     class VARCHAR(20) NOT NULL, 
-    stat TINYINT NOT NULL,
-    PRIMARY KEY (title, uploader)
+    stat BIT(1) NOT NULL,
+    PRIMARY KEY (title, uploader_id),
+    CHECK (price > 0)
 );
